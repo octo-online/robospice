45e86e6d2104dfa9ce907d9f81b76fca3331012d
Stéphane Nicolas <steff.nicolas@gmail.com>
2012-06-23 01:00:44 +0200

    Initial commit

--- android.rest.client/src/main/java/com/octo/android/rest/client/contentmanager/AbstractContentManager.java
+++ android.rest.client/src/main/java/com/octo/android/rest/client/contentmanager/AbstractContentManager.java
@@ -12,7 +12,6 @@ import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.ResultReceiver;
-import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.util.SparseArray;
 import com.octo.android.rest.client.contentmanager.listener.ContentRequestFinishedListener;
