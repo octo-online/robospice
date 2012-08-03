7c04ade63d7d2b777727d3ec9bddbd9565a0b4b1
Stéphane Nicolas <steff.nicolas@gmail.com>
2012-08-03 23:44:14 +0200

    retry

--- android.rest.client/src/com/octo/android/rest/client/ContentManager.java
+++ android.rest.client/src/com/octo/android/rest/client/ContentManager.java
@@ -20,7 +20,8 @@ import com.octo.android.rest.client.request.ContentRequest;
  * Class used to manage content received from web service. <br/>
  * <ul>
  * <li>Start a {@link Service} to request the web service</li>
- * <li>Manage the communication between the Service and the Activity or Fragment : maintains a list of requests and a list of result receiver (listener)</li>
+ * <li>Manage the communication between the Service and the Activity or Fragment : maintains a list of requests and a
+ * list of result receiver (listener)</li>
  * </ul>
  * 
  * @author jva
@@ -28,109 +29,110 @@ import com.octo.android.rest.client.request.ContentRequest;
  */
 public class ContentManager extends Thread {
 
-	private ContentService contentService;
-	private ContentServiceConnection contentServiceConnection = new ContentServiceConnection();
-	private Context context;
+    private ContentService contentService;
+    private ContentServiceConnection contentServiceConnection = new ContentServiceConnection();
+    private Context context;
 
-	private boolean isStopped;
-	private Queue<ContentRequest<?>> requestQueue = new LinkedList<ContentRequest<?>>();
-	private Map<ContentRequest<?>, Boolean> mapRequestToCacheUsageFlag = new HashMap<ContentRequest<?>, Boolean>();
+    private boolean isStopped;
+    private Queue< ContentRequest< ? >> requestQueue = new LinkedList< ContentRequest< ? >>();
+    private Map< ContentRequest< ? >, Boolean > mapRequestToCacheUsageFlag = new HashMap< ContentRequest< ? >, Boolean >();
 
-	private Object lockQueue = new Object();
-	private Object lockAcquireService = new Object();
-	
-	Handler handlerResponse = new Handler();
+    private Object lockQueue = new Object();
+    private Object lockAcquireService = new Object();
 
-	@Override
-	public final synchronized void start() {
-		throw new IllegalStateException( "Can't be started without context.");
-	}
+    Handler handlerResponse = new Handler();
 
-	public synchronized void start(Context context) {
-		this.context = context;
-		super.start();
-	}
+    @Override
+    public final synchronized void start() {
+        throw new IllegalStateException( "Can't be started without context." );
+    }
 
-	public void run() {
-		bindService( context );
+    public synchronized void start( Context context ) {
+        this.context = context;
+        super.start();
+    }
 
-		synchronized (lockAcquireService) {
-			while( contentService == null ) {
-				try {
-					lockAcquireService.wait();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-		}
+    @Override
+    public void run() {
+        bindService( context );
 
-		while( !isStopped ) {
-			if( !requestQueue.isEmpty() ) {
-				ContentRequest<?> restRequest = requestQueue.poll();
-				boolean useCache = mapRequestToCacheUsageFlag.get(restRequest);
-				mapRequestToCacheUsageFlag.remove(restRequest);
-				contentService.addRequest(restRequest, handlerResponse, useCache );
-			}
+        synchronized ( lockAcquireService ) {
+            while ( contentService == null ) {
+                try {
+                    lockAcquireService.wait();
+                } catch ( InterruptedException e ) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
-			synchronized (lockQueue) {
-				while( requestQueue.isEmpty()) {
-					try {
-						lockQueue.wait();
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		}
+        while ( !isStopped ) {
+            synchronized ( lockQueue ) {
+                if ( !requestQueue.isEmpty() ) {
+                    ContentRequest< ? > restRequest = requestQueue.poll();
+                    boolean useCache = mapRequestToCacheUsageFlag.get( restRequest );
+                    mapRequestToCacheUsageFlag.remove( restRequest );
+                    contentService.addRequest( restRequest, handlerResponse, useCache );
+                }
 
-		unbindService(context);
-	}
+                while ( requestQueue.isEmpty() ) {
+                    try {
+                        lockQueue.wait();
+                    } catch ( InterruptedException e ) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
 
-	public void shouldStop() {
-		this.isStopped = true;
-		unbindService(context);
-	}
+        unbindService( context );
+    }
 
-	private void bindService(Context context ) {
-		Intent intentService = new Intent(context, ContentService.class);
-		contentServiceConnection = new ContentServiceConnection();
-		context.bindService(intentService, contentServiceConnection, Context.BIND_AUTO_CREATE);
-	}
+    public void shouldStop() {
+        this.isStopped = true;
+        unbindService( context );
+    }
 
-	private void  unbindService(Context context) {
-		context.unbindService(this.contentServiceConnection);
-	}
+    private void bindService( Context context ) {
+        Intent intentService = new Intent( context, ContentService.class );
+        contentServiceConnection = new ContentServiceConnection();
+        context.bindService( intentService, contentServiceConnection, Context.BIND_AUTO_CREATE );
+    }
 
-	public void addRequestToQueue(ContentRequest<?> request, boolean useCache) {
-		synchronized (lockQueue) {
-			this.requestQueue.add( request);
-			this.mapRequestToCacheUsageFlag.put(request, useCache);
-			lockQueue.notifyAll();
-		}
-	}
+    private void unbindService( Context context ) {
+        context.unbindService( this.contentServiceConnection );
+    }
 
-	public void cancel(ContentRequest<?> request) {
-		request.cancel();
-	}
+    public void addRequestToQueue( ContentRequest< ? > request, boolean useCache ) {
+        synchronized ( lockQueue ) {
+            this.requestQueue.add( request );
+            this.mapRequestToCacheUsageFlag.put( request, useCache );
+            lockQueue.notifyAll();
+        }
+    }
 
-	public class ContentServiceConnection implements ServiceConnection {
+    public void cancel( ContentRequest< ? > request ) {
+        request.cancel();
+    }
 
-		public void onServiceConnected(ComponentName name, IBinder service) {
-			contentService = ((ContentServiceBinder) service).getContentService();
-			synchronized (lockAcquireService) {
-				lockAcquireService.notifyAll();
-			}
-		}
+    public class ContentServiceConnection implements ServiceConnection {
 
-		public void onServiceDisconnected(ComponentName name) {
-			contentService = null;
-		}
-	}
+        public void onServiceConnected( ComponentName name, IBinder service ) {
+            contentService = ( (ContentServiceBinder) service ).getContentService();
+            synchronized ( lockAcquireService ) {
+                lockAcquireService.notifyAll();
+            }
+        }
 
-	public void cancelAllRequests() {
-		for( ContentRequest<?> restRequest : requestQueue ) {
-			restRequest.cancel();
-		}
-	}
+        public void onServiceDisconnected( ComponentName name ) {
+            contentService = null;
+        }
+    }
+
+    public void cancelAllRequests() {
+        for ( ContentRequest< ? > restRequest : requestQueue ) {
+            restRequest.cancel();
+        }
+    }
 
 }
