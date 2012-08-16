d8248e1ac8188c028a5825ec6b2bd3a98abefa15
Stéphane Nicolas <steff.nicolas@gmail.com>
2012-08-16 13:02:11 +0200

    Renamed and test for all file based cache managers

--- content-manager-it/src/com/octo/android/rest/client/persistence/simple/InputStreamCacheManagerTest.java
+++ content-manager-it/src/com/octo/android/rest/client/persistence/simple/InputStreamCacheManagerTest.java
@@ -1,59 +1,59 @@
-//package com.octo.android.rest.client.persistence.simple;
-//
-//import java.io.ByteArrayInputStream;
-//import java.io.ByteArrayOutputStream;
-//import java.io.File;
-//import java.io.FileInputStream;
-//import java.io.FileNotFoundException;
-//import java.io.IOException;
-//
-//import android.test.ActivityInstrumentationTestCase2;
-//
-//import com.google.common.io.ByteStreams;
-//import com.google.common.io.Files;
-//import com.octo.android.rest.client.persistence.CacheExpiredException;
-//import com.octo.android.rest.client.sample.HelloAndroidActivity;
-//
-//public class InputStreamCacheManagerTest extends ActivityInstrumentationTestCase2< HelloAndroidActivity > {
-//
-//    private static final String TEST_CACHE_KEY = "TEST_CACHE_KEY";
-//
-//    private InputStreamCacheManager inputStreamCacheManager;
-//
-//    public InputStreamCacheManagerTest() {
-//        super( "com.octo.android.Rest.client", HelloAndroidActivity.class );
-//    }
-//
-//    @Override
-//    protected void setUp() throws Exception {
-//        super.setUp();
-//        inputStreamCacheManager = new InputStreamCacheManager( getActivity().getApplication() );
-//    }
-//
-//    public void testSaveDataToCacheAndReturnData() throws FileNotFoundException, IOException {
-//        inputStreamCacheManager.saveDataToCacheAndReturnData( new ByteArrayInputStream( "coucou".getBytes() ), TEST_CACHE_KEY );
-//
-//        File cachedFile = inputStreamCacheManager.getCacheFile( TEST_CACHE_KEY );
-//        assertTrue( cachedFile.exists() );
-//
-//        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-//        ByteStreams.copy( new FileInputStream( cachedFile ), bos );
-//        assertEquals( "coucou".getBytes(), bos.toByteArray() );
-//    }
-//
-//    public void testLoadDataFromCache() throws FileNotFoundException, IOException, CacheExpiredException {
-//        File cachedFile = inputStreamCacheManager.getCacheFile( TEST_CACHE_KEY );
-//        Files.write( "coucou".getBytes(), cachedFile );
-//
-//        byte[] actual = Files.toByteArray( cachedFile );
-//        assertEquals( "coucou".getBytes(), actual );
-//    }
-//
-//    @Override
-//    protected void tearDown() throws Exception {
-//        getActivity().finish();
-//        inputStreamCacheManager.removeAllDataFromCache();
-//        super.tearDown();
-//    }
-//
-// }
+package com.octo.android.rest.client.persistence.simple;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.google.common.io.ByteStreams;
+import com.google.common.io.Files;
+import com.octo.android.rest.client.persistence.CacheExpiredException;
+import com.octo.android.rest.client.sample.HelloAndroidActivity;
+
+public class InputStreamCacheManagerTest extends ActivityInstrumentationTestCase2< HelloAndroidActivity > {
+
+    private static final String TEST_CACHE_KEY = "TEST_CACHE_KEY";
+
+    private InputStreamCacheManager inputStreamCacheManager;
+
+    public InputStreamCacheManagerTest() {
+        super( "com.octo.android.Rest.client", HelloAndroidActivity.class );
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        inputStreamCacheManager = new InputStreamCacheManager( getActivity().getApplication() );
+    }
+
+    public void testSaveDataToCacheAndReturnData() throws FileNotFoundException, IOException {
+        inputStreamCacheManager.saveDataToCacheAndReturnData( new ByteArrayInputStream( "coucou".getBytes() ), TEST_CACHE_KEY );
+
+        File cachedFile = inputStreamCacheManager.getCacheFile( TEST_CACHE_KEY );
+        assertTrue( cachedFile.exists() );
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ByteStreams.copy( new FileInputStream( cachedFile ), bos );
+        assertEquals( "coucou".getBytes(), bos.toByteArray() );
+    }
+
+    public void testLoadDataFromCache() throws FileNotFoundException, IOException, CacheExpiredException {
+        File cachedFile = inputStreamCacheManager.getCacheFile( TEST_CACHE_KEY );
+        Files.write( "coucou".getBytes(), cachedFile );
+
+        byte[] actual = Files.toByteArray( cachedFile );
+        assertEquals( "coucou".getBytes(), actual );
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        getActivity().finish();
+        inputStreamCacheManager.removeAllDataFromCache();
+        super.tearDown();
+    }
+
+}
