53cfa007057ded2bc3f03ff1bb47f90fec0bad5a
Jerome Van Der Linden <jeromevdl@gmail.com>
2012-09-19 17:21:38 +0200

    refactoring for github deployment (rename projects)

--- content-manager/src/com/octo/android/rest/client/ContentServiceListener.java
+++
@@ -1,7 +0,0 @@
-package com.octo.android.rest.client;
-
-import com.octo.android.rest.client.request.CachedContentRequest;
-
-public interface ContentServiceListener {
-	public void onRequestProcessed(CachedContentRequest<?> cachedContentRequest);
-}
